d97f7ec29d1afe5f6c21d2beb5d790c72af26649
Yu, Hai Ping <buddy.yu@gmail.com>
2014-03-26 23:57:30 +0800

    Delete 20080828.sql

--- install/20080828.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE `chem_structures` (
-  `id` int(9) unsigned NOT NULL auto_increment,
-  `chem_id` int(9) unsigned NOT NULL,
-  `structure` text NOT NULL,
-  PRIMARY KEY  (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
